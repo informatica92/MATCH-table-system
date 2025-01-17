d708f9f9dc46364ec8038836e35f3fa3c42e8390
Marco Riezzo <marco.riezzo@finconsgroup.com>
2025-01-17 12:48:22 +0100

    improved login page

--- app_pages/99_Login.py
+++ app_pages/99_Login.py
@@ -8,7 +8,13 @@ col_title, col_help = st.columns([9, 1])
 stu.add_title_text(col_title, frmt="{title}")
 stu.add_help_button(col_help)
 
-
-with st.container(border=True, key="login_container"):
-    st.subheader("Login")
-    login_button()
+col_fake1, col_login, col_fake2 = st.columns([2, 4, 2])
+with col_login:
+    with st.container(border=True, key="login_container"):
+        st.subheader("Login")
+        st.write("Please login to access the app")
+        st.write("You can login with your **Auth0** account or with your **Google** account. \n\n"
+                 "If you don't have an account, you can create one by clicking on the **'Sign up'** link. \n\n"
+                 "Once you are logged in, you will be able to access the app pages. \n\n"
+                 "Anyway, **in order to also create, join, edit and delete tables, you also need to set a username**")
+        login_button()
