5cf35f73b5cd9e17c6478296348110d2000c9859
Marco Riezzo <marco.riezzo@finconsgroup.com>
2025-01-21 12:07:01 +0100

    reviewed Login info message

--- app_pages/99_Login.py
+++ app_pages/99_Login.py
@@ -11,10 +11,11 @@ stu.add_help_button(col_help)
 col_fake1, col_login, col_fake2 = st.columns([2, 4, 2])
 with col_login:
     with st.container(border=True, key="login_container"):
-        st.subheader("Login")
+        st.subheader("🛡️ Login")
         st.write("Please login to access the app")
         st.write("You can login with your **Auth0** account or with your **Google** account. \n\n"
                  "If you don't have an account, you can create one by clicking on the **'Sign up'** link. \n\n"
                  "Once you are logged in, you will be able to access the app pages. \n\n"
-                 "Anyway, **in order to also create, join, edit and delete tables, you also need to set a username**")
+                 "Anyway, **in order to also Create**, **Join**, **Edit** and **Delete** tables, **you also need to set"
+                 "an Username** (the app will guide you in doing this)")
         login_button()
