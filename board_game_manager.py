790b8c256d3dedb91e396de3af3abedf0bd8555a
Marco Riezzo <marco.riezzo@finconsgroup.com>
2025-03-07 12:57:55 +0100

    added TODO for expanded expansions list

--- board_game_manager.py
+++ board_game_manager.py
@@ -5,9 +5,10 @@ import utils.streamlit_utils as stu
 from utils.table_system_user import StreamlitTableSystemUser, login_button, logout_button
 
 # # FEATURES
-# TODO: into "View & Join" page add a tertiary button on usernames to shoe the user profile with a dialog
+# TODO: into "View & Join" page add a tertiary button on usernames to show the user profile with a dialog
 # TODO: replace text+bgg search with: https://pypi.org/project/streamlit-searchbox/ (requires no streamlit_extras => no cookies)
 # TODO: add board game image during creation (for check)
+# TODO: add "see N more expansions" in case of 3+ expansions selected (global: all the expansion lists expanded or not)
 # # IMPROVEMENTS
 # TODO: use @st.fragments
 # TODO: optimize return data in BGG info (from tuple to dataclass)
